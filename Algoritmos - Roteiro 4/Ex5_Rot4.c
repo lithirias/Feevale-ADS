6739732da1e40030fa2e027cef02a8e050c61468
Vinícius Marcelo <ermelotp@outlook.com>
2023-10-31 21:25:55 -0300

    Alterado

--- Algoritmos - Roteiro 4/Ex5_Rot4.c
+++ Algoritmos - Roteiro 4/Ex5_Rot4.c
@@ -14,24 +14,45 @@ Lembre-se de garantir que os dados lidos estejam adequados ao problema.
 #include <stdio.h>
 
 int main(){
-    int alunos, aulas, faltas, reprova_faltas = 0;
-    float nmr1, nmr2, nmr3;
+    int aulas, faltas;
+    float alunos, nmr1, nmr2, nmr3, media, reprova_faltas = 0, reprova_notas = 0, aprova = 0;
 
     do{
         printf("\nDigite a quantidade de alunos e aulas ministradas: ");
-        scanf("%d%d", &alunos, &aulas);
+        scanf("%f%d", &alunos, &aulas);
     }while(alunos <= 0 || aulas <= 0);
 
     for(int i = 0; i < alunos; i++){
-        
-
         do{
-            printf("\nDigite o numero de faltas do aluno %d: ", i+1);
-            scanf("%d", &faltas);
-        }while(faltas >= 0 && faltas <= aulas);
+            printf("Digite as tres notas do aluno %d: ", i+1);
+            scanf("%f%f%f", &nmr1, &nmr2, &nmr3);
+        }while((nmr1 > 10 || nmr1 < 0) || (nmr2 > 10 || nmr2 < 0) || (nmr3 > 10 || nmr3 < 0));
 
-        if ((faltas / aulas) > 0.25){
-            reprova_faltas++;
+        if (nmr1 < nmr2 && nmr1 < nmr3)
+            media = (nmr2+nmr3)/2;
+        else
+            if(nmr2 < nmr1 && nmr2 < nmr3)
+                media = (nmr1+nmr3)/2;
+            else
+                media = (nmr1+nmr2)/2;
+
+        if (media >= 7){
+            do{
+                printf("\nDigite o numero de faltas do aluno %d: ", i+1);
+                scanf("%d", &faltas);
+            }while(faltas < 0 && faltas > aulas);
+
+            if ((faltas / aulas) > 0.25){
+                reprova_faltas++;
+            }
+            else
+                aprova++;
         }
+        else
+            reprova_notas++;
     }
+
+    printf("\nAlunos aprovados: %.2f%%", (aprova/alunos)*100);
+    printf("\nAlunos reprovados por nota: %.2f%%", (reprova_notas/alunos)*100);
+    printf("\nAlunos reprovador por falta: %.2f%%", (reprova_faltas/alunos)*100);
 }
